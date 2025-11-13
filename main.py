42eec811b5122826f1f8573b8a798c2d57d5d876
TanyaSab <tsabaeva21@gmail.com>
2025-11-13 12:16:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from datetime import datetime
 import time
+import os
 
 TOKEN = os.getenv("VK_TOKEN")
 GROUP_ID = int(os.getenv("GROUP_ID"))
